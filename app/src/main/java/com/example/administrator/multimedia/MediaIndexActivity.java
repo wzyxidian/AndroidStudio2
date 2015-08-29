6f32da64e9f8e02a6e9522d5a0748d7388a0f584
wzyxidian <wzyxidian@gmail.com>
2015-08-29 23:39:13 +0800

    Android多媒体的目录

---
+++ app/src/main/java/com/example/administrator/multimedia/MediaIndexActivity.java
@@ -0,0 +1,43 @@
+package com.example.administrator.multimedia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.administrator.androidstudio2.R;
+
+public class MediaIndexActivity extends ActionBarActivity {
+    private ButtonClickListeners buttonClickListener;
+    private Button button101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_index);
+        init();
+        buttonClickListener = new ButtonClickListeners();
+        buttonClickListener();
+    }
+
+    private void init(){
+        button101 = (Button) findViewById(R.id.button101);
+
+    }
+
+    private void buttonClickListener() {
+         button101.setOnClickListener(buttonClickListener);
+    }
+    public class ButtonClickListeners implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case  R.id.button101:
+
+
+            }
+        }
+    }
+
+}
+
